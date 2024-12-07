a0524093cfd8278a3ddf61a62b3abe22272c788b
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2024-12-07 21:52:23 +0100

    added 2024 day1

--- years/2024/day01/index.ts
+++ years/2024/day01/index.ts
@@ -1,12 +1,35 @@
 import { join } from "path";
 import { parseFileToRows } from "../../../helpers/fileParser";
 
-export async function run(dir: string): Promise<[number,number]> {
+export async function run(dir: string): Promise<[number, number]> {
   const filePath = join(dir, `${process.env.FILE}.txt`);
-  let input = await parseFileToRows(filePath);
+  let lines = await parseFileToRows(filePath);
 
-  const task1 = 0;
-  const task2 = 0;
+  let left: number[] = [];
+  let right: number[] = [];
 
-  return [task1,task2]
+  for (let line of lines) {
+    let parts = line.split("   ");
+    left.push(parseInt(parts[0]));
+    right.push(parseInt(parts[1]));
+  }
+
+  left.sort();
+  right.sort();
+
+  let task1 = 0;
+  let task2 = 0;
+
+  const occurance = new Map<number, number>();
+
+  for (let i = 0; i < left.length; i++) {
+    task1 += Math.abs(left[i] - right[i]);
+    occurance.set(right[i], (occurance.get(right[i]) || 0) + 1);
+  }
+
+  for (let i = 0; i < left.length; i++) {
+    task2 += left[i] * (occurance.get(left[i]) || 0);
+  }
+
+  return [task1, task2];
 }
