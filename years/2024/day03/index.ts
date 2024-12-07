830b4dd9486e0bc15f5857b8442a63a6ac2cfedb
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2024-12-07 23:53:26 +0100

    added day3

--- years/2024/day03/index.ts
+++ years/2024/day03/index.ts
@@ -1,12 +1,32 @@
 import { join } from "path";
-import { parseFileToRows } from "../../../helpers/fileParser";
+import {
+  parseFileToString
+} from "../../../helpers/fileParser";
 
-export async function run(dir: string): Promise<[number,number]> {
+export async function run(dir: string): Promise<[number, number]> {
   const filePath = join(dir, `${process.env.FILE}.txt`);
-  let input = await parseFileToRows(filePath);
+  let input = await parseFileToString(filePath);
 
-  const task1 = 0;
-  const task2 = 0;
+  const regex = /don't\(\)|do\(\)|mul\((\d+),(\d+)\)/g;
+  const matches = Array.from(input.matchAll(regex));
 
-  return [task1,task2]
+  let task1 = 0;
+  let task2 = 0;
+  let enable = true;
+  for (let match of matches) {
+    if (match[0] === "do()") enable = true;
+    else if (match[0] === "don't()") enable = false;
+    else {
+      const regex = /mul\((\d+),(\d+)\)/;
+      const numbers = match[0].match(regex);
+      if (!numbers) continue;
+      const num1 = parseInt(numbers[1], 10);
+      const num2 = parseInt(numbers[2], 10);
+      const sum = num1 * num2;
+      task1 += sum;
+      task2 += enable === true ? sum : 0;
+    }
+  }
+
+  return [task1, task2];
 }
