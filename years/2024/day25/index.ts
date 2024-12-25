6a6de9940b6cc2b224341f483792532b35f3133a
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2024-12-25 21:35:23 +0100

    added day25

--- years/2024/day25/index.ts
+++ years/2024/day25/index.ts
@@ -1,12 +1,56 @@
 import { join } from "path";
 import { parseFileToRows } from "../../../helpers/fileParser";
 
-export async function run(dir: string): Promise<[number,number]> {
+export async function run(dir: string): Promise<[number, number]> {
   const filePath = join(dir, `${process.env.FILE}.txt`);
-  let input = await parseFileToRows(filePath);
+  let lines = await parseFileToRows(filePath);
 
-  const task1 = 0;
+  const blocks: string[][][] = [];
+  let block: string[][] = [];
+  for (let line of lines) {
+    if (line === "") {
+      blocks.push(block);
+      block = [];
+      continue;
+    }
+
+    block.push(line.split(""));
+  }
+  blocks.push(block);
+
+  let keys: string[][][] = [];
+  let locks: string[][][] = [];
+  for (let block of blocks) {
+    if (block[0].every((e) => e === "#")) {
+      locks.push(block);
+    } else keys.push(block);
+  }
+
+  let score = 0;
+  for (let key of keys) {
+    for (let lock of locks) {
+      const keyHeight = getHeight(key);
+      const lockHeight = getHeight(lock);
+      const check: number[] = [];
+      for (let i = 0; i < keyHeight.length; i++) {
+        check.push(keyHeight[i] + lockHeight[i]);
+      }
+      score += check.every((n) => n <= 5) ? 1 : 0;
+    }
+  }
+
+  const task1 = score;
   const task2 = 0;
 
-  return [task1,task2]
+  return [task1, task2];
+}
+
+function getHeight(block: string[][]): number[] {
+  let heights = [0, 0, 0, 0, 0];
+  for (let y = 0; y < block.length; y++) {
+    for (let x = 0; x < block[0].length; x++) {
+      heights[x] += block[y][x] === "#" ? 1 : 0;
+    }
+  }
+  return heights.map((n) => n - 1);
 }
